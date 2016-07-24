7bc5adbb9fc808b265b457b702b5b60649b23117
arunjoshi08 <joshiarun08@gmail.com>
2016-07-24 19:55:59 +0530

    Initial Project set-up committed

---
+++ MedKat/src/main/java/com/medkat/consumer/MedKatApplication.java
@@ -0,0 +1,12 @@
+package com.medkat.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedKatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedKatApplication.class, args);
+	}
+}
